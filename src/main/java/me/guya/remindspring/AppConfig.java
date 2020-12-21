5e67096ee4b75d1ba17fc32ec98bee0503f420c4
chillgu <bbonggu92@gmail.com>
2020-12-21 15:26:31 +0900

    add jdbc for db connection

--- src/main/java/me/guya/remindspring/AppConfig.java
+++ src/main/java/me/guya/remindspring/AppConfig.java
@@ -1,13 +1,25 @@
 package me.guya.remindspring;
 
+import com.zaxxer.hikari.HikariDataSource;
+import me.guya.remindspring.repository.JdbcMemberRepository;
 import me.guya.remindspring.repository.MemberRepository;
 import me.guya.remindspring.repository.MemoryMemberRepository;
 import me.guya.remindspring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class AppConfig {
+
+    private final DataSource dataSource;
+
+    @Autowired
+    public AppConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -17,6 +29,7 @@ public class AppConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        // return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
